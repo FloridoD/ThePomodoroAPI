7f57118f2701313b76b9619b9568f8e7bc161dc4
Diogo Flórido <diogoflorido2000@gmail.com>
2021-04-17 23:10:08 +0100

    Revert "pesquisa"
    This reverts commit e62822e00e8dec782835cf680193f9c77e230266.

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
@@ -16,7 +15,7 @@ import datetime
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = str(uuid.uuid1())
+app.config['SECRET_KEY'] = 'secretkey' #str(uuid.uuid1())
 api = Api(app)
 
 def token_required(f):
@@ -48,7 +47,7 @@ class Database:
     def __init__(self):
         connection = psycopg2.connect(user = "ylhmlqpfkqbwxu",password = "86acc7cb14978bd57697eaa59022eec26a08f1930662da86502de3e39fd30e0d",host = "ec2-54-247-158-179.eu-west-1.compute.amazonaws.com",port = "5432",database = "dadih75qcq1ih")
         self.connection=connection
-        self.ingredientes = self.getAllIngredients()
+
     def query(self, query_str):
         cursor = self.connection.cursor()
         cursor.execute(query_str)
@@ -85,6 +84,17 @@ class Database:
             self.connection.commit()
             return jsonify({'message': 'Username already exists'})
 
+    def getFriends(self, user_data):
+
+        return ''
+
+    def getFriendsRecipes(self, user_data):
+
+        return ''
+
+    def getLikedRecipes(self, user_data):
+
+        return ''
 
     def editUser(self,username,user_data):
         cursor = self.connection.cursor(cursor_factory=RealDictCursor)
@@ -103,12 +113,11 @@ class Database:
         try:
             query = f"SELECT id,username,name,bio,image FROM person WHERE username = '{username}';"
             results = self.query(query)
-            data = results["results"][0]
 
             if results is None:
                 return None
             else:
-                return data
+                return results.get("results")[0]
         except Exception:
             return None
 
@@ -135,7 +144,7 @@ class Database:
             return jsonify({"message" : "User not Found"})
 
     def getUsersLastNRecipes(self, username, ini, fim):
-        query = "SELECT id,name,post_date,rating,image FROM recipe WHERE person_id = (SELECT id FROM person WHERE username = \'%s\');"%(username)
+        query = "SELECT id,name,post_date,description,rating,image FROM recipe WHERE person_id = (SELECT id FROM person WHERE username = \'%s\');"%(username)
         
         recipes = self.query(query)
 
@@ -154,7 +163,7 @@ class Database:
     def addRecipe(self, username, recipe_data):
         cursor = self.connection.cursor()
         try:
-            ingredientes = json.loads(recipe_data['ingredients'])
+            ingredientes = recipe_data['ingredients']
             query = f"INSERT INTO recipe (name, image, description, preparation, post_date, person_id) values('{recipe_data['name']}', '{recipe_data['image']}', '{recipe_data['description']}', '{recipe_data['preparation']}', CURRENT_TIMESTAMP, (SELECT id from person where username = '{username}')) RETURNING id"
             cursor.execute(query)
             id_of_new_row = cursor.fetchone()[0]
@@ -173,17 +182,18 @@ class Database:
         cursor = self.connection.cursor(cursor_factory=RealDictCursor)
         query = "INSERT INTO ingredient (name) values "
         for i in ingredientes:
-            query+="('"+i+"'),"
+            query += f"('{i.get('name')}'),"
+
         query = query[:-1]+" ON CONFLICT (name) DO NOTHING;"
         cursor.execute(query)
-        print(query)  
         self.connection.commit()
+        print(query)  
+
         query = "INSERT INTO recipe_ingredient (recipe_id,ingredient_id,quantity) values "
         for i in ingredientes:
-            query+="(%s,(SELECT id FROM ingredient WHERE name = \'%s\'),\'%s\'),"%(recipe_id,i,ingredientes[i])
+            query += f"({recipe_id},(SELECT id FROM ingredient WHERE name = '{i.get('name')}'),'{i.get('quantity')}'),"  
         query = query[:-1]+";"
         cursor.execute(query)
-        print(query)  
         self.connection.commit()
 
     def deleteRecipe(self,recipe_data):
@@ -209,59 +219,7 @@ class Database:
         cursor = self.connection.cursor(cursor_factory=RealDictCursor)
         cursor.execute("SELECT recipe_ingredient.ingredient_id, ingredient.name, recipe_ingredient.quantity FROM ingredient, recipe_ingredient WHERE recipe_ingredient.recipe_id = \'%s\' AND recipe_ingredient.ingredient_id = ingredient.id;"%(recipe_id))
         return cursor.fetchall()
-    def getRecipesFromIngredients(self,ingredientes):
-        cursor = self.connection.cursor(cursor_factory=RealDictCursor)
-        query = "SELECT recipe_id, ingredient_id, recipe FROM recipe_ingredient, recipe WHERE ingredient_id IN (SELECT ingredient.id FROM ingredient WHERE LOWER(ingredient.name) IN ("
-        for i in ingredientes:
-            query+="'"+i+"',"
-        query = query[:-1] + ")) AND recipe_id = id;"
-        cursor.execute(query)
-        print(query)
-        dic = {}
-        out = {}
-        for i in cursor.fetchall():
-            if i["recipe_id"] in dic:
-                dic[i["recipe_id"]]+= [i["ingredient_id"]]
-            else:
-                dic[i["recipe_id"]] = [i["recipe"]]
-        for i in dic:
-            if len(ingredientes) == len(dic[i]):
-                out[i] = dic[i][0]
-        if out:
-            return jsonify(out)
-        else:
-            return jsonify({'message': 'Sorry, no recipes found'})
-    def getAllIngredients(self):
-        cursor = self.connection.cursor(cursor_factory=RealDictCursor)
-        cursor.execute("SELECT name, COUNT(ingredient_id) FROM ingredient, recipe_ingredient WHERE id = ingredient_id GROUP BY name ORDER BY -COUNT(ingredient_id);")
-        k = 1000
-        if cursor.rowcount < 1000:
-            k = cursor.rowcount
-        out = []
-        cu = cursor.fetchall()[:k]
-        for i in cu:
-            out += [i["name"]]
-        out.sort()
-        return out
-    def getIngredientsFromText(self, query):
-        res = [i for i in self.ingredientes if query in i]
-        return jsonify(res)
-    def getRecipeFromText(self, query):
-        cursor = self.connection.cursor()
-        cursor.execute("SELECT recipe FROM recipe WHERE name LIKE '%"+query+"%';")
-        return jsonify(cursor.fetchall())
-    def rateRecipe(self,username,rate_data):
-        try:
-            query = 'INSERT INTO rating (rate, rate_date, recipe_id, person_id) values ('+rate_data['rate']+',CURRENT_TIMESTAMP,'+rate_data['recipe_id']+',(SELECT id FROM person WHERE username = \''+username+'\'));'
-            cursor.execute(query)
-            self.connection.commit()
-            cursor.execute('UPDATE recipe SET rating = rating*(1-1/(rates+1)) + %s*(1/(rates+1)), rates = rates + 1;'%(rate_data['rate']))
-            self.connection.commit()
-            return jsonify({'message': 'Rating Inserted'})
-        except Exception as e:
-            print(e)
-            self.connection.commit()
-            return jsonify({'message': 'Error'})
+
 class Login(Resource):
     def __init__(self,database):
         self.database = database
@@ -307,24 +265,10 @@ class Recipe(Resource):
     def delete(self):
         return self.database.deleteRecipe(request.form)
 
-class SearchByIngredients(Resource):
-    def __init__(self,database):
-        self.database = database
-    def post(self):
-        return self.database.getRecipesFromIngredients(request.get_json()['ingredients'])
-    def get(self):
-        return self.database.getIngredientsFromText(request.get_json()['query'])
-class SearchByName(Resource):
-    def __init__(self,database):
-        self.database = database
-    def post(self):
-        return self.database.getRecipeFromText(request.get_json()['query'])
-
 database = Database()
 api.add_resource(Login, '/login',resource_class_args=(database,))
 api.add_resource(User, '/user',resource_class_args=(database,))
 api.add_resource(Recipe, '/recipe',resource_class_args=(database,))
-api.add_resource(SearchByIngredients, '/search_by_ingredients',resource_class_args=(database,))
-api.add_resource(SearchByName, '/search_by_name',resource_class_args=(database,))
+
 if __name__ == "__main__": 
     app.run(debug=True)
